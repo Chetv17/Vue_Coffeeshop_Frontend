6661e8d70637ca584f94f13605a8db9f0cf3fe9c
Chester Vincent <chetvincent@chesters-mbp-2.mynetworksettings.com>
2023-01-13 10:18:29 -0500

    basic page setup

--- vue-coffeeshop/src/App.vue
+++ vue-coffeeshop/src/App.vue
@@ -1,10 +1,22 @@
 <template>
+  <div class='container'>
+    <Header />
+  </div>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
 </template>
+<script>
+import Header from './components/Header'
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ vue-coffeeshop/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <h1>Big Tree Coffee Roasters</h1>
+  </header>
+</template>
+<script>
+export default {
+  name: 'TitleHeader'
+}
+</script>
+<style scoped>
+  header {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          margin-bottom: 20px;
+      }
+</style>
